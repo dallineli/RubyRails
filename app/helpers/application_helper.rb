35bc8d2ff613a9b83c0edc696a30feffdc38d6c6
Dallin Eliason <dallineliason@Dallins-MacBook-Pro.local>
2018-02-19 17:07:53 -0700

    Added styles to sign up page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
-	def login_helper 
+	def login_helper style 
 		 if current_user.is_a?(GuestUser) 
-        (link_to "Sign Up", new_user_registration_path, class: 'nav-link') +
+        (link_to "Sign Up", new_user_registration_path, class: style) +
       " ".html_safe +
-        (link_to "Login", new_user_session_path, class: 'nav-link') 
+        (link_to "Login", new_user_session_path, class: style) 
      else         
-     link_to "Logout", destroy_user_session_path, method: :delete, class: 'nav-link'
+     link_to "Logout", destroy_user_session_path, method: :delete, class: style
      end
     end
      
-    def source_helper
+    def source_helper(layout_name)
       if session[:source] 
-         greeting = "Thanks for visiting me from #{session[:source]}"
+         greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout."
          content_tag(:p, greeting, class: "source-greeting")
      end
    end 
